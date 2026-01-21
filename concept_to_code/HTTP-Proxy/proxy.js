721b1d02288e73fc1d06d1b48e3e6079dd095e86
Yubin Lee <yubin8938@gmail.com>
2026-01-21 20:54:30 +0900

    React 사용 구구단 게임 구현 (#56)

--- concept_to_code/HTTP-Proxy/proxy.js
+++ concept_to_code/HTTP-Proxy/proxy.js
@@ -15,12 +15,8 @@ class Proxy {
   constructor() {
     this.tcpServer = undefined;
     this.clientSocket = undefined;
-    this.requestMessage = "";
     this.httpRequest = new HttpRequest();
     this.httpResponse = new HttpResponse();
-
-    this.requestMessage = "";
-    this.responseMessage = "";
   }
 
   run() {
@@ -32,6 +28,7 @@ class Proxy {
   initServer() {
     this.tcpServer = net.createServer((clientSocket) => {
       this.clientSocket = clientSocket;
+      clientSocket.write("요청 입력 > \r\n");
       this.#printClientInfo(clientSocket);
       this.#getRequestMessage(clientSocket);
     });
@@ -44,89 +41,93 @@ class Proxy {
     });
   }
 
-  #parseRequestMessage(requestMsg) {
-    const method = requestMsg.match(REQUEST_PARSING_REGEX.METHOD)[1].trim();
-    const accept = requestMsg.match(REQUEST_PARSING_REGEX.ACCEPT)[1].trim();
-    const host = requestMsg.match(REQUEST_PARSING_REGEX.HOST)[1].trim();
-    if (!host) {
-      throw new Error("Host 미포함");
-    }
-    const userAgent = requestMsg
-      .match(REQUEST_PARSING_REGEX.USER_AGENT)[1]
-      .trim();
-    //parse 한 request를 할당
-    this.httpRequest.setUp(method, host, userAgent, accept);
-
-    this.resolveHostname(host);
-  }
-
   //파싱한 host name으로 DNS 서버에서 IP주소 가져오기
-  resolveHostname(hostName) {
+  resolveHostname(hostName, requestMessage) {
     dns.resolve4(hostName, (err, addr) => {
       if (err) {
         throw new Error("잘못된 형식의 Host Name");
       }
       const IP = addr[0];
-      console.log(IP);
-      this.sendRequestToUpstreamServer(IP);
+      this.sendRequestToUpstreamServer(IP, requestMessage);
     });
   }
 
-  sendRequestToUpstreamServer(IP) {
+  sendRequestToUpstreamServer(IP, requestMessage) {
     const serverSocket = new net.Socket();
     this.#connectToUpstreamServer(IP, serverSocket);
-    this.#sendRequestMessage(serverSocket);
+    this.#sendRequestMessage(serverSocket, requestMessage);
     this.#getResponseMessage(serverSocket);
   }
 
-  sendResponseToClient() {
-    this.clientSocket.write(this.responseMessage);
+  sendResponseToClient(responseMessage) {
+    this.clientSocket.write(`서버 응답 >\r\n ${responseMessage}`);
+  }
+  #parseRequestMessage(requestMessage) {
+    const method = requestMessage.match(REQUEST_PARSING_REGEX.METHOD)[1].trim();
+    const accept = requestMessage.match(REQUEST_PARSING_REGEX.ACCEPT)[1].trim();
+    const host = requestMessage.match(REQUEST_PARSING_REGEX.HOST)[1].trim();
+
+    if (!host) {
+      throw new Error("Host 미포함");
+    }
+    const userAgent = requestMessage
+      .match(REQUEST_PARSING_REGEX.USER_AGENT)[1]
+      .trim();
+    //parse 한 request를 할당
+    this.httpRequest.setUp(method, host, userAgent, accept);
+
+    console.log(this.httpRequest);
+    this.resolveHostname(host, requestMessage);
   }
 
   #parseResponseMessage(responseMsg) {
-    const statusCode = responseMsg.match(RESPONSE_PARSING_REGEX.STATUS_CODE);
-    const responseLine = responseMsg.match(
-      RESPONSE_PARSING_REGEX.RESPONSE_LINE
-    );
-    const contentLength = responseMsg.match(
-      RESPONSE_PARSING_REGEX.CONTENT_LENGTH
-    );
-    const body = responseMsg.match(RESPONSE_PARSING_REGEX.BODY);
+    const statusCode = responseMsg
+      .match(RESPONSE_PARSING_REGEX.STATUS_CODE)[1]
+      .trim();
+    const responseLine = responseMsg
+      .match(RESPONSE_PARSING_REGEX.RESPONSE_LINE)[1]
+      .trim();
+    const contentLength = responseMsg
+      .match(RESPONSE_PARSING_REGEX.CONTENT_LENGTH)[1]
+      .trim();
+    const body = responseMsg.match(RESPONSE_PARSING_REGEX.BODY)[1].trim();
 
     this.httpResponse.setUp(statusCode, responseLine, contentLength, body);
+
+    console.log(this.httpResponse);
   }
 
   #connectToUpstreamServer(IP, socket) {
     socket.connect(DEFAULT_PORT, IP);
   }
 
-  #sendRequestMessage(socket) {
-    socket.write(this.requestMessage);
+  #sendRequestMessage(socket, requestMessage) {
+    socket.write(requestMessage);
   }
 
   #getResponseMessage(socket) {
     socket.on("data", (responseBuffer) => {
-      this.responseMessage = responseBuffer.toString();
-      this.#parseResponseMessage(this.responseMessage);
+      const responseMessage = responseBuffer.toString();
+      this.#parseResponseMessage(responseMessage);
+
+      this.sendResponseToClient(responseMessage);
     });
   }
 
   #printClientInfo(socket) {
     console.log(`<new client>
-      IP: ${socket.remoteAddress}, port: ${socket.remotePort}`);
+      IP: ${socket.remoteAddress}, port: ${socket.remotePort}\r\n`);
   }
 
   #getRequestMessage(socket) {
+    let requestMessage = "";
     socket.on("data", (requestBuffer) => {
-      this.requestMessage += requestBuffer.toString();
+      requestMessage += requestBuffer.toString();
 
       //줄바꿈이 있는 경우- 줄바꿈 이전의 텍스트만 남기고 parse 함수를 호출
-      if (this.requestMessage.includes("\r\n\r\n")) {
-        this.requestMessage = this.requestMessage.replace(
-          AFTER_HEADER_REGEX,
-          ""
-        );
-        this.#parseRequestMessage(this.requestMessage);
+      if (requestMessage.includes("\r\n\r\n")) {
+        requestMessage = requestMessage.replace(AFTER_HEADER_REGEX, "");
+        this.#parseRequestMessage(requestMessage);
       }
     });
   }
